2d47c1d920bf71e18f67ea1efe730ed93f79fb00
Andy <andy@Andys-MacBook-Pro.local>
2022-12-02 19:53:37 -0500

    EventCard start

---
+++ src/components/EventCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: true,
+//   },
+// })
+</script>
+
+<template>
+  <div class="greetings"></div>
+</template>
+
+<style scoped></style>
