d170cc3b8c625908e4125efb1be5fa50ec132286
Andy <andy@Andys-MacBook-Pro.local>
2022-12-02 21:07:27 -0500

    EventCard

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,14 +1,43 @@
 <script setup>
+import { ref } from 'vue'
+
 // defineProps({
 //   msg: {
 //     type: String,
 //     required: true,
 //   },
 // })
+
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee',
+})
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
