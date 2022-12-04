585bf86cc862abacd3f16851d0222b613420dcd5
Andy <andy@Andys-MacBook-Pro.local>
2022-12-03 21:58:57 -0500

    h1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,6 +40,7 @@ const events = ref([
 </script>
 
 <template>
+  <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
