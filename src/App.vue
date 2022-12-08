729061474e4168f48e73628ddf3a16b6900967ae
Andy <andy@Andys-MacBook-Pro.local>
2022-12-08 15:05:07 -0500

    rename Home link

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Events</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
