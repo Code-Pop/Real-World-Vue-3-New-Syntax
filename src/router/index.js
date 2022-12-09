f03fafe0ce641094ff7c80d0b41b8fd5f218bb8d
Andy <andy@Andys-MacBook-Pro.local>
2022-12-09 00:45:13 -0500

    remove comment

--- src/router/index.js
+++ src/router/index.js
@@ -13,9 +13,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: AboutView,
     },
   ],
