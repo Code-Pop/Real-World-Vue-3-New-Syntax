b312a232f86b9ca79a8ad3c4cfaac74ba6bd8176
Andy <andy@Andys-MacBook-Pro.local>
2022-12-10 17:23:58 -0500

    EventDetailsView template

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
