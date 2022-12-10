3450f4b955572242921b800bed9052d3a7b85f0f
Andy <andy@Andys-MacBook-Pro.local>
2022-12-10 17:25:50 -0500

    EventService.getEvent(id)

--- src/views/EventDetailsView.vue
+++ src/views/EventDetailsView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService.js'
 
 const event = ref(null)
+const id = ref(123)
 
-onMounted(() => {})
+onMounted(() => {
+  EventService.getEvent(id.value)
+    .then((response) => {
+      console.log(response)
+      event.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 </script>
 
 <template>
