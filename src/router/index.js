bd5358ae47a333898fc68de84113f7bea9b04d12
Andy <andy@Andys-MacBook-Pro.local>
2022-12-10 17:44:23 -0500

    add event-details to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView,
+    },
+    {
+      path: '/event/123',
+      name: 'event-details',
+      component: EventDetailsView,
     },
     {
       path: '/about',
