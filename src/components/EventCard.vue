5ff3b584d6377c68b9aee4b85c0de2874f61792d
Andy <andy@Andys-MacBook-Pro.local>
2022-12-10 21:15:08 -0500

    event-link style

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,7 +8,10 @@ defineProps({
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'event-details', params: { id: event.id } }">
+  <RouterLink
+    class="event-link"
+    :to="{ name: 'event-details', params: { id: event.id } }"
+  >
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
@@ -27,5 +30,9 @@ defineProps({
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
